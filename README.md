f7da63510118d45d2a5a3da26036ae61f077318c
Dagan <daganlev@gmail.com>
2022-09-18 15:11:42 +0100

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # scbuild
 Node JS build to compile, minify and generate source maps for TS and SCSS
+
+## Installation
+Copy `node.js` and `package.json` files to an empty directory and run below line to install packages: 
+```
+npm install
+```
+
+Create a `ts` folder and a `scss` folder in the main root and store your typescript files in the `ts` folder and your SCSS files in the `scss` folder.
+
+When the build command is executed the system will loop through all TS files in the `ts` folder, transpile those to JS and minify them - generating a **.js** file in a new `js` directory and a **.js.map** file next to it.
+
+It will do the same to the scss files and put them in a `css` folder - it will skip over any _xxxx.scss files (files with an underscore at the beginning of their name) - so those can be included within other scss files
+
+To run the build use the below command:
+```
+npm start
+```
+
+The output will produce a list of all compiled files, their sizes and how long it took to process them.
+
+In case of a fatal error the script will exit; on SCSS errors the console will show a red error message, on TS errors the system will not create the new .js file.
---
+++ node.js
@@ -0,0 +1,120 @@
+const fs = require('fs');
+const glob = require('glob');
+const ts = require('typescript');
+const UglifyJS = require('uglify-js');
+const sass = require('sass');
+const postcss = require('postcss');
+const cssnano = require('cssnano');
+
+let scriptsRunning = false;
+let stylesRunning = false;
+
+//init
+updateStyles();
+updateScripts();
+
+fs.watch('./ts',{},function(evt, file){
+    if(!scriptsRunning)
+        updateScripts();
+});
+
+fs.watch('./scss',{},function(evt, file){
+    if(!stylesRunning)
+        updateStyles();
+});
+
+function updateScripts(){
+    scriptsRunning = true;
+    
+    fs.rmSync('./js', { recursive: true, force: true }); // clean up directory
+    fs.mkdirSync('./js');
+
+    glob("./ts/*.ts", null, function (er, files) {
+        files.forEach(file => {
+            let startTime = performance.now();
+
+            let jsfilename = file.replace("./ts/","./js/").replace(".ts",".js");
+
+            data = fs.readFileSync(file,'utf8');
+            
+            let tmpRes = ts.transpile(data, {
+                "target": "es2016",
+                "module": "commonjs",
+                "sourceMap": true,
+                "inlineSourceMap": true
+            }, file);
+
+            let [tempResJS, tempResSource] = tmpRes.split("//# sourceMappingURL=data:application/json;base64,");
+            tempResSource = Buffer.from(tempResSource, 'base64').toString('utf8') ;
+            tempResSource = tempResSource.replace('"sources":[', '"sources":[".' + file + '",');
+
+            let tmpFile = UglifyJS.minify(tempResJS,  { 
+                compress: true,
+                mangle: true,
+                sourceMap: {
+                    content: tempResSource,
+                    url: jsfilename.replace("./js/","") + '.map'
+                }
+            });
+            
+            fs.writeFileSync(jsfilename, tmpFile.code);
+            fs.writeFileSync(jsfilename + '.map', tmpFile.map);
+
+            let diff = ((performance.now() - startTime)/1000).toFixed(3);
+            let size = (fs.statSync(jsfilename).size / 1024).toFixed(2);
+            console.log(jsfilename + ' \x1b[32mupdated\x1b[0m ' + size + ' KB :: ' + diff + 'sec');
+            
+        });
+        scriptsRunning = false;
+    });
+}
+
+function updateStyles(){
+    stylesRunning = true;
+    
+    fs.rmSync('./css', { recursive: true, force: true }); // clean up directory
+    fs.mkdirSync('./css');
+
+    glob("./scss/*.scss", null, function (er, files) {
+        files.forEach(file => {
+            //skip scss files with underscore at the start of their name (i.e. _global.scss)
+            if(!/scss\/\_/.test(file)){
+                let startTime = performance.now();
+                try {
+                    let res = sass.compile(file, {
+                        style: 'compressed',
+                        sourceMap: true
+                    });
+
+                    //update sources with relative path
+                    let tmpSrcs = [];
+                    res.sourceMap.sources.forEach(elm => {
+                        let tmpEl = elm.split("/");
+                        tmpSrcs.push("../scss/" + tmpEl[tmpEl.length-1]);
+                    });
+                    res.sourceMap.sources = tmpSrcs;
+
+                    postcss([cssnano]).process(res.css,  {
+                        from: null, 
+                        to: null,
+                        map: {
+                            prev: JSON.stringify(res.sourceMap)
+                        }
+                    }).then(result => {
+                        let cssfilename = file.replace("./scss/","./css/").replace(".scss",".css");
+                        fs.writeFileSync(cssfilename, result.css + "\n/*# sourceMappingURL=" + cssfilename.replace("./css/","") + ".map */");
+                        fs.writeFileSync(cssfilename + '.map', result.map.toString());
+                        let diff = ((performance.now() - startTime)/1000).toFixed(3);
+                        let size = (fs.statSync(cssfilename).size / 1024).toFixed(2);
+                        console.log(cssfilename + " \x1b[32mupdated\x1b[0m " + size + ' KB :: ' + diff + 'sec');
+                    
+                    });
+                } catch (error) {
+                    console.error(file + " \x1b[31merror\x1b[0m");
+                }
+                
+            }           
+        });
+        stylesRunning = false;
+    });
+}
